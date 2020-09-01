511915579374e042f0cefb41b5dfe9839ff64328
rochellev <rochellevroberts@utexas.edu>
2020-09-01 13:55:34 -0700

    add name property for profile

--- src/features/profiles/profilesSlice.tsx
+++ src/features/profiles/profilesSlice.tsx
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-{ managingUserID: '1', profilePic: 'profile profile', coverPic: 'hello cover', gallery:'gallery array', rememberedCommunity: 'list of users'
+{ managingUserID: '1', rememberedName:'Bobby Draper', profilePic: 'profile profile', coverPic: 'hello cover', gallery:'gallery array', rememberedCommunity: 'list of users'
 },
-{ managingUserID: '2', profilePic: 'profile profile!', coverPic: 'hello cover!', gallery:'gallery array', rememberedCommunity: 'list of users'
+{ managingUserID: '2', rememberedName:'Bobby2 Draper', profilePic: 'profile profile!', coverPic: 'hello cover!', gallery:'gallery array', rememberedCommunity: 'list of users'
 }
 ]
 
