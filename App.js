9bfadd784aacc2f5ba9bcea55c207530aab51412
rochellev <rochellevroberts@utexas.edu>
2020-09-09 14:30:54 -0700

    add navigation container wrapper around whole app

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
+import "react-native-gesture-handler";
 import React from "react";
-// import { getStore } from "./src/app/store";
+import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { ProfileView } from "./src/profiles/ProfileView";
 import Constants from "expo-constants";
-import {getStore} from './store'
+import { getStore } from "./store";
 
 export default function App() {
   return (
-    <Provider store={getStore()}>
-      <SafeAreaView style={styles.container}>
-        <ProfileView id={10} />
-        <View></View>
-      </SafeAreaView>
-    </Provider>
+    <NavigationContainer>
+      <Provider store={getStore()}>
+        <SafeAreaView style={styles.container}>
+          <ProfileView id={10} />
+          <View></View>
+        </SafeAreaView>
+      </Provider>
+    </NavigationContainer>
   );
 }
 
