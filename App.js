4fe231fe993a984cbb3f7cd1c93ebb00529d8174
rochellev <rochellevroberts@utexas.edu>
2020-09-10 10:24:08 -0700

    add test screens to set up navigation

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { Provider } from "react-redux";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { ProfileView } from "./src/profiles/ProfileView";
 import Constants from "expo-constants";
 import { getStore } from "./store";
 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
       <Provider store={getStore()}>
-        <SafeAreaView style={styles.container}>
-          <ProfileView id={10} />
-          <View></View>
-        </SafeAreaView>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
       </Provider>
     </NavigationContainer>
   );
